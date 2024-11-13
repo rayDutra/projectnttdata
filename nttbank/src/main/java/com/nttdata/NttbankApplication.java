efa959bf643ef4a563a21f08d3af408c568b57f4
Rayanne Dutra <rayanneoliveiradutra@gmail.com>
2024-11-13 11:30:29 -0300

    realizado criaçao projeto

---
+++ nttbank/src/main/java/com/nttdata/NttbankApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttbankApplication.class, args);
+	}
+
+}
